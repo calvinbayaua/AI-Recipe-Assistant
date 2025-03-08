41106da9f067a05c43d1614e3f78233b10cbfa26
calvinbayaua <38298812+calvinbayaua@users.noreply.github.com>
2025-03-07 16:46:54 -0800

    add user input form

---
+++ src/components/Main.jsx
@@ -0,0 +1,30 @@
+export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const ingredientsList = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    ingredients.push(newIngredient);
+    console.log(newIngredient);
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>{ingredientsList}</ul>
+    </main>
+  );
+}
