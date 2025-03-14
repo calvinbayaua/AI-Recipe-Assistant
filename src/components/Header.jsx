36621662044d2285e24a5e4618923b2cb7da3834
calvinbayaua <38298812+calvinbayaua@users.noreply.github.com>
2025-03-14 12:42:02 -0700

    -Refactored Main.jsx into separate components (IngredientsList.jsx and ClaudeRecipe.jsx)
    -Connected to MistralAI LLM via huggingface inference API in ai.js
    
    -User's specified ingredients used as input to a function call to MistralAI
    
    -Formatted response using ReactMarkdown and displayed as completed recipe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Claude from "../assets/Chef Claude Icon.png";
 export default function Header() {
   return (
     <header>
-      <img src={Claude} className='claude-img' alt="claude img"></img>
+      <img src={Claude} className="claude-img" alt="claude img"></img>
       <h1>Chef Claude</h1>
     </header>
   );
